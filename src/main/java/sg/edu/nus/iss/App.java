7d9e0dd3a335120c77608f653d4ffdc0851f04a5
LeeMeiLing <leem0060@outlook.com>
2023-01-17 15:54:27 +0800

    finished client

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,10 +1,13 @@
 package sg.edu.nus.iss;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.Console;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
@@ -17,28 +20,45 @@ public final class App {
 
     /**
      * Says hello to the world.
+     * 
      * @param args The arguments of the program.
      */
-    public static void main(String[] args) throws IOException{
-        
-        Socket socket = new Socket("localhost",7000);
+    public static void main(String[] args) throws IOException {
 
-        try(OutputStream os = socket.getOutputStream()){
+        Socket socket = new Socket("localhost", 7000);
+
+        try (OutputStream os = socket.getOutputStream()) {
             BufferedOutputStream bos = new BufferedOutputStream(os);
             DataOutputStream dos = new DataOutputStream(bos);
 
             Console con = System.console();
             String readInput = "";
+            String receivedMsg = "";
 
-            while(!readInput.equalsIgnoreCase("close")){
-                readInput = con.readLine("Enter a command: ");
-                dos.writeUTF(readInput);
-                dos.flush();
+            try (InputStream is = socket.getInputStream()) {
 
+                BufferedInputStream bis = new BufferedInputStream(is);
+                DataInputStream dis = new DataInputStream(bis);
+
+                while (!readInput.equalsIgnoreCase("close")) {
+                    readInput = con.readLine("Enter a command: ");
+                    dos.writeUTF(readInput);
+                    dos.flush();
+                    
+                    receivedMsg = dis.readUTF();
+                    System.out.println(receivedMsg);
+                }
+
+                dis.close();
+                bis.close();
+                is.close();
+
+            } catch (EOFException ex) {
+                socket.close();
             }
 
-        }catch(EOFException ex){
-            socket.close();
+        } catch (EOFException ex) {
+            socket.close();cd
         }
 
     }
